284d358c4944d29e7a5f72fd501eb4a4fc1ee1d8
Dipali Ahire <ahire.dipali5@gmail.com>
2022-06-14 13:24:33 +0530

    Initial commit

---
+++ DigitalHorizonsUITask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DigitalHorizonsUITask
+//
+//  Created by Dipali Rajbanshi on 14/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
